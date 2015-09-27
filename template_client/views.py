4e0fe8768b1504c808ff56e1705904a01cb70907
Alvin Wan <hi@alvinwan.com>
2015-09-26 22:25:33 -0700

    added homepage for test

--- template_client/views.py
+++ template_client/views.py
@@ -1,11 +1,15 @@
-from flask import jsonify, Blueprint
+from flask import jsonify, Blueprint, url_for
 from .libs.sample import Sample
 
 
 public = Blueprint('public', __name__)
 
-@public.route('/create')
-def create():
+@public.route('/')
+def index():
+    return '<a href="%s">test logic connection</a>' % url_for('public.test')
+
+@public.route('/test')
+def test():
     """Creates a new Sample object and displays all objects"""
     Sample(option='c').post()
     Sample(option='d').post()
