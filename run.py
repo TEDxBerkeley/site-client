3846ccb4c69c67b8c460076290ad4c70b3a5a379
Alvin Wan <hi@alvinwan.com>
2015-10-11 10:34:57 -0700

    renamed app and dir

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 # the main Flask application
-from template_client import create_template_app
+from site_client import create_site_app
 
-app = create_template_app()
+app = create_site_app()
 
 if __name__ == "__main__":
 	app.run(**app.config['INIT'])
